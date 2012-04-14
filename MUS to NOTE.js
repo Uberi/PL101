3408c5a1fe34f733c6143f31ce9010218d784deb
Anthony Zhang <azhang9@gmail.com>
2012-04-14 16:37:12 -0400

    Add tests.

--- MUS to NOTE.js
+++ MUS to NOTE.js
@@ -19,3 +19,17 @@ var compile = function (musexpr, time) {
         return compile(musexpr.left,time).concat(compile(musexpr.right,time));
     }
 };
+
+var melody_mus = 
+    { tag: 'seq',
+      left: 
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+console.log(melody_mus);
+console.log(compile(melody_mus));
